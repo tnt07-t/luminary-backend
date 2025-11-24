2232d51a76f947703a170a52b880759fe549c50f
tran <tranntran007@gmail.com>
2025-11-23 19:51:04 -0500

    setup

--- src/app.py
+++ src/app.py
@@ -1,2 +1,43 @@
-# hello!
-#hi this is fixed??
+import json
+
+from db import Course, Assignment, User , db
+from flask import Flask, request
+
+app = Flask(__name__)
+db_filename = "cms.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+#initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
