68231271420842fe03c89c6e82f742c808b0e600
tran <tranntran007@gmail.com>
2025-11-23 20:08:03 -0500

    added User class and started Constellation_Attempts class

--- src/db.py
+++ src/db.py
@@ -6,12 +6,39 @@ db = SQLAlchemy()
 
 #Database Model Classes
 class User():
-    
+    """
+    User model
+    One-to-many relationship with Constellations_attempts
+    One-to-many relationship with Study_sessions
+    """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     display_name = db.Column(db.String, nullable = False)
+
+
+    def __init__(self, **kwargs):
+        """
+        Initialize Course object
+        """
+        self.display_name = kwargs.get("display_name")
 
     def serialize(self):
         """
         Serialize a User Object
         """
+        return {
+            "id" : self.id,
+            "display_name" : self.display_name
+        }
+    
+    def simple_serialize(self):
+        return {
+            "id" : self.id,
+            "display_name" : self.display_name
+        }
+
+
+class Constellation_Attempts(self):
+    """
+    Constellation_Attempts model
+    """
