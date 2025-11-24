6ee051b1a6520f402ab511889fea4e98c712c9d5
tran <tranntran007@gmail.com>
2025-11-23 19:59:14 -0500

    doing users

--- src/db.py
+++ src/db.py
@@ -1,3 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+
+#Database Model Classes
+class User():
+    
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    display_name = db.Column(db.String, nullable = False)
+
+    def serialize(self):
+        """
+        Serialize a User Object
+        """
